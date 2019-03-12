7ac77e9ae557d3606d405e72724c1a634802395f
Oleksii Yashchuk <YashchukAlex5@gmail.com>
2019-03-12 12:50:40 +0200

    Remove MVC from project

--- MyForum_Backend/Global.asax.cs
+++ MyForum_Backend/Global.asax.cs
@@ -16,8 +16,6 @@ namespace MyForum_Backend
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
