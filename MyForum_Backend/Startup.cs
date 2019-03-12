2a561a41055bda2d01552bbb53dfff86246a4829
Oleksii Yashchuk <YashchukAlex5@gmail.com>
2019-03-12 12:25:43 +0200

    Add project files.

---
+++ MyForum_Backend/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MyForum_Backend.Startup))]
+
+namespace MyForum_Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
