358b9b711e6e5e2bbcf1591e18ebc59fcf986328
Oleksii Yashchuk <YashchukAlex5@gmail.com>
2019-03-16 00:02:06 +0200

    add StatusController

---
+++ MyForum_Backend/Controllers/StatusController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MyForum_Backend.Models;
+using MyForum_Backend.Models.DB_Models;
+
+namespace MyForum_Backend.Controllers
+{
+    [RoutePrefix("api/Status")]
+    public class StatusController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Status
+        [HttpGet]
+        [Route("")]
+        public IQueryable<Status> GetStatuses()
+        {
+            return db.Statuses;
+        }
+
+        // GET: api/Status/{id}
+        [HttpGet]
+        [Route("{id}")]
+        [ResponseType(typeof(Status))]
+        public async Task<IHttpActionResult> GetStatus(int id)
+        {
+            Status status = await db.Statuses.FindAsync(id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(status);
+        }
+
+        // PUT: api/Status/{id}
+        [HttpPut]
+        [Route("{id}")]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutStatus(int id, Status status)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != status.StatusID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(status).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StatusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Status
+        [HttpPost]
+        [Route("")]
+        [ResponseType(typeof(Status))]
+        public async Task<IHttpActionResult> PostStatus(Status status)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Statuses.Add(status);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = status.StatusID }, status);
+        }
+
+        // DELETE: api/Status/{id}
+        [HttpDelete]
+        [Route("{}")]
+        [ResponseType(typeof(Status))]
+        public async Task<IHttpActionResult> DeleteStatus(int id)
+        {
+            Status status = await db.Statuses.FindAsync(id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+
+            db.Statuses.Remove(status);
+            await db.SaveChangesAsync();
+
+            return Ok(status);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StatusExists(int id)
+        {
+            return db.Statuses.Count(e => e.StatusID == id) > 0;
+        }
+    }
+}
